d7e2cdc33b73c51b2d52b694639bb6bb4f742823
Aman Kumar <akjha1469@gmail.com>
2024-02-01 01:18:22 +0530

    auth

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import dbConfig from "./db/dbConfig.js";
+
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -11,6 +14,8 @@ dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
 	dbConfig();
