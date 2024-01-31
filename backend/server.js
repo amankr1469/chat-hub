04872947e64aa81278a256404f93850e68b10745
Aman Kumar <akjha1469@gmail.com>
2024-02-01 00:33:29 +0530

    db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
-const express = require("express");
 
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import dbConfig from "./db/dbConfig.js";
 const app = express();
 
-app.listen(5000, () => {
-	console.log(`Server Running on port 5000`);
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+	dbConfig();
+	console.log(`Server Running on port ${PORT}`);
 });
