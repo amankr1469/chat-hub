735bf6eb2e59b93af1580c9b88db799dc6d7357c
Aman Kumar <akjha1469@gmail.com>
2024-02-03 11:35:27 +0530

    UI

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,8 +5,9 @@ export const getUsersForSidebar = async (req, res) => {
 		const loggedInUserId = req.user._id;
 
 		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
-
-		res.status(200).json(filteredUsers);
+        //{ $ne: loggedInUserId } This will allow us to not get LoggedIn user in the name, so we dont get our own user in the sidebar
+		
+        res.status(200).json(filteredUsers);
 	} catch (error) {
 		console.error("Error in getUsersForSidebar: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
